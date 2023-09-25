e5973c1b5a5e44a873033a3ba4b9c66a114d505a
Roma <romanvolskiy791@gmail.com>
2023-09-25 21:28:35 +0300

    added about page

---
+++ src/views/About.vue
@@ -0,0 +1,28 @@
+<script setup>
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+</script>
+
+<template>
+  <Header/>
+  <section>
+    <h2>Our Story</h2>
+    <p>Welcome to our About Us page. Here, you can learn more about our company and its history.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod lorem nec tellus tristique, vel tempus tortor tincidunt. Duis non libero auctor, posuere nisl eget, tempor tellus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla facilisi.</p>
+  </section>
+
+  <section>
+    <h2>Our Team</h2>
+    <p>Meet our dedicated team members who work hard to make our company successful.</p>
+    <ul>
+      <li>John Doe - CEO</li>
+      <li>Jane Smith - COO</li>
+      <li>Bob Johnson - CTO</li>
+    </ul>
+  </section>
+  <Footer/>
+</template>
+
+<style scoped>
+
+</style>
