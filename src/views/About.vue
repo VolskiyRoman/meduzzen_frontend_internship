ce21768bbb66ab89dd8d8623e25322b510aefc02
Roma <romanvolskiy791@gmail.com>
2023-10-09 17:50:18 +0300

    added axios

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,10 +1,22 @@
 <script setup>
+import {ref} from "vue";
+import instance from "../../api/api";
 import Header from "@/components/Header.vue";
 import Footer from "@/components/Footer.vue";
+const apiResponse = ref(null);
+
+instance.get('')
+    .then(response => {
+      apiResponse.value = JSON.stringify(response.data);
+    })
+    .catch(error => {
+      console.error('API Error:', error);
+    });
 </script>
 
 <template>
   <Header/>
+  <div>{{ apiResponse }}</div>
   <div>
     <h1>{{ $t("components.aboutPage") }}</h1>
     <p>{{ $t("components.aboutPageContent") }}</p>
