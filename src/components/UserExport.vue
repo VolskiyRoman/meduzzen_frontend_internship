795d640906526771b48d8657ef8af013d72144e9
Volskiy Roman <romanvolskiy791@gmail.com>
2023-11-20 12:56:48 +0200

    fixes

--- src/components/UserExport.vue
+++ src/components/UserExport.vue
@@ -5,19 +5,11 @@
 </template>
 
 <script setup>
-import axiosInstance from "@/api/api";
+import { exportData } from "@/services/export_data";
 
 const exportUser = async () => {
   try {
-    const response = await axiosInstance.get('/api/quizzes/user-export/', { responseType: 'blob' });
-
-    const url = window.URL.createObjectURL(new Blob([response.data]));
-    const link = document.createElement('a');
-    link.href = url;
-    link.setAttribute('download', 'user_export.csv');
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+    await exportData(null, 'quizzes/user-export', 'user', null);
   } catch (error) {
     console.error('Error exporting user:', error);
   }
